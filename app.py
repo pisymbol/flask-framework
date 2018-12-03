7adc307676c5c3975e9d656a6b74e72b0ff9d307
Alexander Sack <pisymbol@gmail.com>
2018-12-03 20:44:41 +0000

    Initial check-in of Ticker Lookup App
    This is my first stab emulating the demo of the
    'Ticker Lookup App' outlined in the 12-day preparation
    guide. It is a required milestone that must be completed
    before the actual course starts.
    
    Not knowing any of these technologies specifically, I followed
    a few online tutorials on Flask, bokeh, and Heroku and used
    them to base my first draft on.
    
    One major difference is that TDI recommend using quandl for
    getting stock data. Unfortunately, quandl has stopped maintaining
    any community driven (read: free) current dataset. Instead, I
    opted to use the new Alpha Vantage API which is updated daily!
    
    So far my testing has only been running Flask locally.
    I have not tested this on Heroku but I suspect it will just
    work (gulp!). I also have not tested multiple browsers.

--- app.py
+++ app.py
@@ -1,14 +1,137 @@
+# The Data Incubator: 12-Day Program
+#
+# Day 10 Milestone: Stock Data Lookup Demo
+#
+# Author: Alexander Sack
+
+import io
+import pandas as pd
+import re
+import requests
+
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
+
 from flask import Flask, render_template, request, redirect
+
+# Quandl is no longer maintaining any up to free date datasets.
+# Use Alpha Vantage instead which is updated daily. We also get
+# the ability to download in CSV format which makes importimg into
+# pandas easier.
+ALPHA_VANTAGE_KEY = 'EHXSSM9A86COI0Z3'
+AV_API_URL = 'https://www.alphavantage.co/query?function={0}&symbol={1}&outputsize={2}&datatype={3}&apikey=' + ALPHA_VANTAGE_KEY
+
+# Yahoo Finance! API to convery stock ticker to company name.
+YAHOO_FINANCE_API_URL = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en'
 
 app = Flask(__name__)
 
-@app.route('/')
+def ticker2name(ticker):
+    """ Convert ticker to company name using Yahoo! Finance API """
+
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(ticker)
+    r = requests.get(url)
+    if r.status_code == 200:
+        js = r.json()
+        result = js['ResultSet']['Result'][0]
+        if result['symbol'] == ticker:
+            return result['name']
+    return ""
+
+def alpha_vantage(ticker, function='TIME_SERIES_DAILY', outputsize='compact', datatype='csv'):
+    """
+    Fetch stock data from Alpha Vantage API
+
+    FIXME: We should probably cache the data to make response time
+    faster but for now we fetch directly from their web API on every
+    form submit. Boo!
+    """
+
+    df = None
+    r = requests.get(AV_API_URL.format(function, ticker, outputsize, datatype))
+    if r.status_code == 200:
+        # AV has a weird thing that if you ask for an invalid ticker, the RPC
+        # still returns 200! So we check if 'Error Message' is in the text to
+        # indicate something bad happened.
+        if re.search(r'Error Message', r.text):
+            return df
+
+        # Have a CSV, read in as a dataframe
+        sio = io.StringIO(r.text)
+        if datatype == 'csv':
+            df = pd.read_csv(sio)
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    return df
+
+def create_figure(df, ticker, lines):
+    """ Create Bokeh figure """
+
+    plot_script = ""
+    plot_div = ""
+
+    # Various figure mappings
+    line_map = {
+            'open' : {'color' : 'blue', 'legend' : 'Open'},
+            'close' : { 'color': 'red', 'legend' : 'Adj Close'},
+            'high' : { 'color' : 'orange', 'legend' : 'High'},
+            'low' : { 'color' : 'black', 'legend' : 'Low'},
+    }
+
+    name = ticker2name(ticker)
+    title = ticker + ' (' + name + ') '
+    source = ColumnDataSource(df)
+    hoverTools = []
+    p = figure(plot_width=800, plot_height=300, x_axis_type="datetime", title=title)
+
+    # Based on what the user requested build lines and hovertools
+    for line in lines:
+        if line in df:
+            p.line(x='timestamp', y=line, source=source,
+                    legend=line_map[line]['legend'], color=line_map[line]['color'])
+            hoverTools.append((line_map[line]['legend'], '@' + line))
+    p.add_tools(HoverTool(tooltips=hoverTools))
+    p.yaxis[0].formatter = NumeralTickFormatter(format="$0.00")
+    p.grid.grid_line_color = None
+
+    # Return the HTML components needed to embed in our template.
+    script, div = components(p)
+
+    return script, div
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  return render_template('index.html')
+    # Static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+    plot_script = ""
+    plot_div = ""
+
+    # Parse form data
+    if request.method == 'POST':
+        ticker = request.form.get('stock')
+        close = request.form.get('close')
+        opening = request.form.get('open')
+        high = request.form.get('high')
+        low = request.form.get('low')
+
+        # Go get stock info
+        df = alpha_vantage(ticker)
+        if df is not None:
+            plot_script, plot_div = create_figure(df, ticker, [opening, close, high, low])
+
+    # Render our results (if any)
+    html = render_template('index.html', js_resources=js_resources, css_resources=css_resources,
+            plot_script=plot_script, plot_div=plot_div)
+
+    return encode_utf8(html)
 
 @app.route('/about')
 def about():
   return render_template('about.html')
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507, debug=True) # Heroku reserved port for flask applications
