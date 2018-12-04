b555bd2d43481b8a221fad5eded7472a4221b102
Alexander Sack <pisymbol@gmail.com>
2018-12-04 03:22:35 +0000

    Create ticker registery and embed RSS feed
    As an added bonus, instead of just a free form text field for
    the ticker symbok use a discrete <datalist> element by dynamically
    creating a stock ticker registry from what is published on the
    Nasdaq site (covers NYSE and BATS as well).
    
    Also, add the latest news headlines for the stock ticker in question
    to the page via the Yahoo! Finance RSS feed.
    
    Flash a short error message too if we can't find the user entered
    ticker symbol (note: I allow for lowercase and extraneous whitespace
    within the datalist text field).

--- app.py
+++ app.py
@@ -4,10 +4,12 @@
 #
 # Author: Alexander Sack
 
+import feedparser
 import io
 import pandas as pd
 import re
 import requests
+import urllib
 
 from bokeh.embed import components
 from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
@@ -15,7 +17,7 @@ from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for, Markup
 
 # Quandl is no longer maintaining any up to free date datasets.
 # Use Alpha Vantage instead which is updated daily. We also get
@@ -24,8 +26,17 @@ from flask import Flask, render_template, request, redirect
 ALPHA_VANTAGE_KEY = 'EHXSSM9A86COI0Z3'
 AV_API_URL = 'https://www.alphavantage.co/query?function={0}&symbol={1}&outputsize={2}&datatype={3}&apikey=' + ALPHA_VANTAGE_KEY
 
-# Yahoo Finance! API to convery stock ticker to company name.
+# Yahoo Finance! APIs 
 YAHOO_FINANCE_API_URL = 'http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en'
+YAHOO_FINANCE_RSS_URL = 'http://finance.yahoo.com/rss/headline?s={0}'
+
+# Nasdaq keeps a fairly up to date list of all major stock tickers
+NASDAQ_SYMBOL_FTP = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/'
+NASDAQ_OTHER = 'otherlisted.txt'
+NASDAQ_LISTED = 'nasdaqlisted.txt'
+
+# Global list of ticker symbols
+TICKERS = []
 
 app = Flask(__name__)
 
@@ -40,6 +51,17 @@ def ticker2name(ticker):
         if result['symbol'] == ticker:
             return result['name']
     return ""
+
+def get_news(ticker):
+    """ Get recent headlines from Yahoo! Finance RSS Feed """
+
+    # Again, getting the ticker's latest news is optional and thus non-fatal
+    # if the RSS feed is down for whatever reason.
+    try:
+        feed = feedparser.parse(YAHOO_FINANCE_RSS_URL.format(ticker))
+        return feed['entries']
+    except:
+        pass
 
 def alpha_vantage(ticker, function='TIME_SERIES_DAILY', outputsize='compact', datatype='csv'):
     """
@@ -109,10 +131,11 @@ def index():
     css_resources = INLINE.render_css()
     plot_script = ""
     plot_div = ""
+    articles = ""
 
     # Parse form data
     if request.method == 'POST':
-        ticker = request.form.get('stock')
+        ticker = request.form.get('ticker').strip().upper()
         close = request.form.get('close')
         opening = request.form.get('open')
         high = request.form.get('high')
@@ -122,10 +145,14 @@ def index():
         df = alpha_vantage(ticker)
         if df is not None:
             plot_script, plot_div = create_figure(df, ticker, [opening, close, high, low])
+            articles = get_news(ticker)
+        else:
+            flash('Stock ticker not found.')
+            return redirect(url_for('index'))
 
     # Render our results (if any)
     html = render_template('index.html', js_resources=js_resources, css_resources=css_resources,
-            plot_script=plot_script, plot_div=plot_div)
+            plot_script=plot_script, plot_div=plot_div, articles=articles, entries=TICKERS)
 
     return encode_utf8(html)
 
@@ -133,5 +160,29 @@ def index():
 def about():
   return render_template('about.html')
 
+def tickers_init():
+    """ Dynamically build a stock ticker catalog """
+
+    # NOTE: Failure is perfeclty fine. This is strictly for convenience.
+    for txt in [NASDAQ_LISTED, NASDAQ_OTHER]:
+        try:
+            file_txt, headers = urllib.request.urlretrieve(NASDAQ_SYMBOL_FTP + '/' + txt)
+            if file_txt:
+                with open(file_txt, 'r') as f:
+                    # Last line is just a useless 'File Creation' entry
+                    for line in f.readlines()[:-1]:
+                        TICKERS.append(line.split('|')[0].strip())
+                os.unlink(file_txt)
+        except:
+            pass
+
+    if TICKERS:
+        sorted(TICKERS)
+
 if __name__ == '__main__':
+    tickers_init()
+
+    # FIXME: Move to separate settings file.
+    app.secret_key = 'tdi'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(port=33507, debug=True) # Heroku reserved port for flask applications
